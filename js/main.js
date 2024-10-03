269a7d95f1e88fb0c3c68b0c26bcb3abaa309ebc
Steve-code16 <stevemolele@yahoo.com>
2024-10-03 22:36:35 +0200

    finalised modal functionality with javascript

--- js/main.js
+++ js/main.js
@@ -94,3 +94,69 @@ readMoreBtns.forEach((readMoreBtn, i) => {
     modalOpen(i);
   });
 });
+
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+  modalCloseBtn.addEventListener("click", () => {
+    header.classList.remove("hide");
+    sliderNav.classList.remove("hide");
+    mainMediaLinks.classList.remove("hide");
+
+    modalContainer.classList.remove("active");
+
+    modalBoxes.forEach((modalBox) => {
+      modalBox.classList.remove("active");
+    });
+
+    modalContents.forEach((modalContent) => {
+      modalContent.classList.remove("active");
+    });
+  });
+});
+
+const swiperSlides = document.querySelectorAll(".swiper-slide");
+
+swiperSlides.forEach((swiperSlide) => {
+  const readMoreBtn = swiperSlide.querySelector(".read-more-btn");
+
+  readMoreBtn.addEventListener("click", () => {
+    const startingElement = document.querySelector(
+      ".swiper-slide.swiper-slide-active"
+    );
+
+    if (startingElement) {
+      const elements = Array.from(document.querySelectorAll(".swiper-slide"));
+      const startingIndex = elements.indexOf(startingElement);
+      const elementToHide = elements.slice(startingIndex);
+
+      elementToHide.forEach((element, index) => {
+        setTimeout(() => {
+          element.classList.add("hide");
+        }, index * 150);
+      });
+    }
+  });
+});
+
+modalBoxes.forEach((modalBox) => {
+  const modalCloseBtn = modalBox.querySelector(".modal-close-btn");
+
+  modalCloseBtn.addEventListener("click", () => {
+    const startingElement = document.querySelector(
+      ".swiper-slide.swiper-slide-active"
+    );
+
+    if (startingElement) {
+      const elements = Array.from(document.querySelectorAll(".swiper-slide"));
+      const startingIndex = elements.indexOf(startingElement);
+      const elementsToShow = elements.slice(startingIndex);
+
+      elementsToShow.forEach((element, index) => {
+        setTimeout(() => {
+          element.classList.remove("hide");
+        }, index * 150);
+      });
+    }
+  });
+});
